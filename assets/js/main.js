bef435d28fa168e9412da44c636ed28b0de8a563
damir.grgic@students.fhnw.ch <damir.grgic@students.fhnw.ch>
2019-01-09 20:31:31 +0100

    jQuery install, includes of nav etc., memes

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+function main() {
+    //importHeader();
+    importNavbar();
+    importFooter();
+    sideNavCollapse();
+
+}
+
+function importHeader() {
+    $("head").load("../rhywaelle/structure/header.html");
+    console.log("loaded header");
+}
+function importFooter() {
+    $("footer").load("../rhywaelle/structure/footer.html");
+    console.log("loaded footer");
+}
+function importNavbar() {
+    $("#nav").load("../rhywaelle/structure/navbar.html");
+    console.log("loaded nav");
+}
+function sideNavCollapse() {
+    $('.sidenav').sidenav();
+    console.log("loaded sidenav");
+}
+
+$(document).ready(main);
